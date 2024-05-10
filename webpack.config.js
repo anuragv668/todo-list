6c7dfafff22e865a86138e5c680d7a19ba9633cf
anuragv668 <anuragvarma668@gmail.com>
2024-05-10 12:55:19 +0530

    config webpack server

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,15 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+  devServer: {
+    static: './dist'
   },
   devtool: 'inline-source-map',
   module: {
