abf3cc84607613d0d8428f56c5951eead0213b9b
anuragv668 <anuragvarma668@gmail.com>
2024-05-18 20:08:44 +0530

    refactor printTask function

--- src/index.js
+++ src/index.js
@@ -23,6 +23,7 @@ const editDialog = content.querySelector('.editDialog');
 
 const form = content.querySelector('form'); //formData
 
+// setting date input 
 const date = document.getElementById('date');
 const today = new Date().toISOString().split('T')[0];
 date.setAttribute('min', today);
@@ -33,39 +34,58 @@ const taskList = content.querySelector('.taskList'); //list for all tasks
 const addTaskBtn = content.querySelector('.addtask'); //button for adding task
 addTaskBtn.addEventListener("click", () => {dialog.showModal()});
 
-function printTasks(arr, givenContainer) {
-  for (let i = 0; i < arr.length; i++) {
-    const taskContainer = document.createElement('div');
-    taskContainer.classList.add('taskContainer');
-    const priority = document.createElement('div');
-    priority.classList.add(arr[i]['priority']);
-    taskContainer.appendChild(priority);
+//sets priority for a task
+const setPriority = (container, obj) => {
+  const priority = document.createElement('div');
+  priority.classList.add(obj['priority']);
+  container.appendChild(priority);
+};
 
-    const dateCont = document.createElement('div');
-    dateCont.textContent = arr[i]['dueDate'];
-    taskContainer.appendChild(dateCont);
+//sets date for a task
+const setDueDate = (container, obj) => {
+  const dateCont = document.createElement('div');
+  dateCont.textContent = obj['dueDate'];
+  container.appendChild(dateCont);
+};
 
-    const task = document.createElement('div');
-    task.textContent = arr[i]['title'];
-    taskContainer.appendChild(task);
-    
+const setTitle = (container, obj) => {
+  const task = document.createElement('div');
+  task.textContent = obj['title'];
+  container.appendChild(task);
+};
+
+const setDescription = (container, obj) => {
     const desc = document.createElement('div');
     desc.textContent = "Description";
     desc.addEventListener('click', () => {
       descDialog.showModal();
-      descDialog.querySelector('.descriptionBody').textContent = arr[i]['description'];
+      descDialog.querySelector('.descriptionBody').textContent = obj['description'];
       descDialog.querySelector('.closeDescription').addEventListener('click', () => {descDialog.close()});
     });
-    taskContainer.appendChild(desc); 
+    container.appendChild(desc);
+};
 
-    const deleteTask = document.createElement('div');
-    deleteTask.textContent = 'Delete';
-    deleteTask.addEventListener('click', () => {
-      logic.removeFromList(i);
-      emptyTasks(givenContainer);
-      printTasks(arr, givenContainer);
-    });
-    taskContainer.appendChild(deleteTask);
+const setDelete = (container, obj, index, givenContainer) => {
+  const deleteTask = document.createElement('div');
+  deleteTask.textContent = 'Delete';
+  deleteTask.addEventListener('click', () => {
+    logic.removeFromList(index);
+    emptyTasks(givenContainer);
+    printTasks(obj, givenContainer);
+  });
+  container.appendChild(deleteTask);
+};
+
+function printTasks(arr, givenContainer) {
+  for (let i = 0; i < arr.length; i++) {
+    const taskContainer = document.createElement('div');
+    taskContainer.classList.add('taskContainer');
+
+    setPriority(taskContainer, arr[i]);
+    setDueDate(taskContainer, arr[i]); 
+    setTitle(taskContainer, arr[i]); 
+    setDescription(taskContainer, arr[i]);
+    setDelete(taskContainer, arr, i, givenContainer); 
 
     givenContainer.appendChild(taskContainer);
   }
